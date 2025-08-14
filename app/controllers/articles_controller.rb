bcdd78a9c54cb7f1ec861ecbfba12e06ad7c4fe9
ayon037 <mohiuddin.310799@gmail.com>
2025-08-14 12:43:52 +0600

    Back to the previous

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,15 +14,15 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated."
-        redirect_to article_path(@article)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
     else
-        render :edit
+      render :edit
     end
   end
 
   def create
-    
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created."
       redirect_to article_path(@article)
